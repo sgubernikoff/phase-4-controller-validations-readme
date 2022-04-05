47299db81faf30e30da6ab10e3f6666bf0eb07b2
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-04-05 12:34:42 -0400

    commit tree 89842dc0746c37c32f4c74d5b4c115e13e63107f

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,8 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
   # GET /birds
   def index
     birds = Bird.all
@@ -45,6 +47,10 @@ class BirdsController < ApplicationController
 
   def render_not_found_response
     render json: { error: "Bird not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
